045e99d21896043b745761ab825f51974bf99ce3
Alan Jansen Situmorang <143371272+alanjnsn@users.noreply.github.com>
2023-09-23 17:15:01 +0700

    Add files via upload

---
+++ program6.py
@@ -0,0 +1,10 @@
+dict = {'Name':'Zara', 'Age':7, 'Class':'First'}
+del dict['Name']
+print(dict)
+dict.clear()
+print(dict)
+del dict
+print(dict)
+print("dict['Age']:", dict['Age'])
+print("dict['School']:", dict['School'])
+
